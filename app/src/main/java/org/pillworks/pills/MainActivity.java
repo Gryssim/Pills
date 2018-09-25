8f37b8e985f53ace978bff68f4b855b4ac3a4037
Doug Plock <dgplock@gmail.com>
2018-09-25 17:22:32 -0600

    Messing around with button attributes. Discovered changing layout displayed buttons properly.
    Added new activity AddNewRx. Need to work on how it brings up new activity. Currently back button closes app?

--- app/src/main/java/org/pillworks/pills/MainActivity.java
+++ app/src/main/java/org/pillworks/pills/MainActivity.java
@@ -2,6 +2,7 @@ package org.pillworks.pills;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //what needs to be initialized on app start?
+        // Probably gather db entries and create array.
+        // InitApp(); //Create DB connection? not sure how this will all work.
+        // InitRxData();
+    }
+
+    public void AddNewRx(View view){
+        setContentView(R.layout.add_new_rx);
+
     }
 }
