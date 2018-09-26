fe528a19a2b48c38045605b809ba92885104d960
Tippy <combatlms@gmail.com>
2018-09-26 11:50:36 -0600

    added implementation button functionality starts new activities

--- app/src/main/java/org/pillworks/pills/MainActivity.java
+++ app/src/main/java/org/pillworks/pills/MainActivity.java
@@ -1,13 +1,43 @@
 package org.pillworks.pills;
-
+import android.widget.Button;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+
 
 public class MainActivity extends AppCompatActivity {
-
+private static Button addRx_btn;
+private static Button editRx_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        OnClickButtonListener();
+    }
+    public void OnClickButtonListener() {
+        addRx_btn = (Button)findViewById(R.id.addRx);
+        addRx_btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("org.pillworks.pills.addNewRx");
+
+                        startActivity(intent);
+                    }
+                }
+        );
+        editRx_btn = (Button)findViewById(R.id.editRx);
+        editRx_btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("org.pillworks.pills.editRx");
+
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
